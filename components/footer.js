9ebb6fd86fed2bd18240413a80379cf963e8b8fa
yug damor <yugdamor.dev@gmail.com>
2022-02-26 00:01:20 +0530

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return <div className="bg-white text-center text-indigo-500 pt-10 text-sm dark:text-slate-500 dark:bg-black">
-      <h1>© 2022 WordBook. All rights reserved.</h1>
+      <h1>© 2022 UpToWord. All rights reserved.</h1>
   </div>;
 };
 

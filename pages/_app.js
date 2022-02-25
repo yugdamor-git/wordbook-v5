477ad6f1f99295a7c2945b3750f5f2122ef580e5
Yug Damor <yugdamor.dev@gmail.com>
2022-02-25 19:00:53 +0530

    progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import Layout from '../components/layout'
 import '../styles/globals.css'
+import NextNProgress from 'nextjs-progressbar';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Layout><Component {...pageProps} /></Layout>
+
+  return <Layout>
+    <NextNProgress
+    options={{ easing: 'ease', speed: 500,showSpinner: false }}
+    color="#6468fc"
+    startPosition={0.3}
+    stopDelayMs={200}
+    height={3}
+    showOnShallow={false}
+  />
+    <Component {...pageProps} />
+    
+    </Layout>
 }
 
 export default MyApp
